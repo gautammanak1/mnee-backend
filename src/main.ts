79d1758f6b9d50917c150bff1bacc9a5efb1ead1
ashish chanchal <akchanchal2002@gmail.com>
2025-11-07 07:34:30 +0530

    use it

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
@@ -11,7 +11,7 @@ async function bootstrap() {
   
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(cookieParser());
+
   app.enableCors({
     origin: '*',
     credentials: true,
